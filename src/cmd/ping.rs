b39dfe2e47840653acc69225876eb3cc678ded3d
Telman Rzaev <rzaev.telman.tarik@gmail.com>
2023-10-15 12:27:22 +0300

    feat: add round trip statistics.
    Close #8

--- src/cmd/ping.rs
+++ src/cmd/ping.rs
@@ -1,6 +1,7 @@
 use crate::net::icmp::{self, Packet};
 use crate::net::rawsock::RawSocket;
 
+use std::collections::LinkedList;
 use std::fmt;
 use std::net::SocketAddr;
 use std::net::ToSocketAddrs;
@@ -23,6 +24,56 @@ pub struct Ping {
 
     buff: Vec<u8>,
     round_trip: Duration,
+}
+
+struct RoundTripStat {
+    stat: LinkedList<Duration>,
+}
+
+impl RoundTripStat {
+    fn new() -> Self {
+        RoundTripStat {
+            stat: LinkedList::new(),
+        }
+    }
+
+    fn push(&mut self, round_trip: Duration) {
+        self.stat.push_back(round_trip)
+    }
+    fn len(&self) -> usize {
+        self.stat.len()
+    }
+}
+
+impl fmt::Display for RoundTripStat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.stat.is_empty() {
+            return write!(f, "",);
+        }
+
+        let def_dur = Duration::from_millis(0);
+
+        let avg: Duration = self.stat.iter().sum::<Duration>() / (self.stat.len() as u32);
+        let min = self.stat.iter().min().unwrap_or(&def_dur);
+        let max = self.stat.iter().max().unwrap_or(&def_dur);
+
+        let stddev: f64 = self
+            .stat
+            .iter()
+            .map(|x| (x.as_millis() as f64 - avg.as_millis() as f64).powf(2.0))
+            .sum::<f64>()
+            / self.stat.len() as f64;
+        let stddev = stddev.sqrt();
+
+        write!(
+            f,
+            "round-trip min/avg/max/stddev = {}/{}/{}/{:.2} ms",
+            min.as_millis(),
+            avg.as_millis(),
+            max.as_millis(),
+            stddev
+        )
+    }
 }
 
 impl fmt::Display for Ping {
@@ -51,14 +102,15 @@ impl Ping {
         let hops = 56;
         let timeout = Duration::from_secs(1);
         let sock = RawSocket::new_icmp(hops, timeout.as_secs() as u32)?;
-        let size = 64;
+        let size = 56;
+        let wait = Duration::from_secs(1);
 
         Ok(Ping {
             sock,
             id: process::id(),
             host: addr,
             host_name: host_name.to_string(),
-            wait: Duration::from_secs(2),
+            wait,
             seq: 0,
             size,
             buff: vec![0; SIZE_OF_IP_V4_PACK + icmp::HDR_BYTE_SIZE + size],
@@ -79,18 +131,43 @@ impl Ping {
         );
         let ctrl_c_events = ctrl_channel()?;
         let ticks = tick(self.wait);
+        let mut stat = RoundTripStat::new();
+        let mut transmitted: u32 = 0;
+        let mut received: u32 = 0;
 
         loop {
             select! {
                 recv(ticks) -> _ => {
-                    // TODO: correctly handle the error
-                    self.echo()?;
-                    // TODO: gather statistics
+                    transmitted+= 1;
+                    let round_trip = match self.echo() {
+                        Ok(dur) => dur,
+                        Err(err) =>  {
+                            eprintln!("{}", err);
+                            continue;
+                        },
+                    };
+                    stat.push(round_trip);
+                    received+=1;
                 }
                 recv(ctrl_c_events) -> _ => {
+                    let received = received as f32;
+                    let transmitted = transmitted as f32;
+
+                    let loss = if transmitted == 0.0 {
+                        0.0
+                    } else {
+                        100.0*(transmitted - received)/transmitted
+                    };
                     println!();
                     println!("--- {} ping statistics ---", self.host_name);
-                    // TODO: print statistics
+                    println!(
+                        "{} packets transmitted, {} packets received, {:.2} packet loss",
+                        transmitted, received,
+                        loss,
+                    );
+                    if stat.len() != 0 {
+                        println!("{}", stat);
+                    }
                     break;
                 }
             }
